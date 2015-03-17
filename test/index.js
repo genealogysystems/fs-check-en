11dcc0aea39de57cdff761dde4ad85ecd551b9fb
Justin <justincyork@gmail.com>
2015-03-17 19:59:10 +0000

    finish existing checks

--- test/index.js
+++ test/index.js
@@ -3,21 +3,21 @@ var data = require('../'),
 
 describe('fs-check-en', function(){
     
-    it('proper number of checks', function(){
-        expect(Object.keys(data).length).to.equal(27);
-    })
-    
-    it('all checks have a title and description', function(){
-        for(var name in data){
-            try {
-                expect(data[name].title).to.have.length.above(1);
-                expect(data[name].description).to.have.length.above(1);
-                expect(data[name].description).to.contain('\n');
-            } catch(e) {
-                console.error('Error validating ' + name);
-                throw(e);
-            }
-        }
-    })
+  it('proper number of checks', function(){
+    expect(Object.keys(data).length).to.equal(38);
+  })
+  
+  it('all checks have a title and description', function(){
+    for(var name in data){
+      try {
+        expect(data[name].title).to.have.length.above(1);
+        expect(data[name].description).to.have.length.above(1);
+        expect(data[name].description).to.contain('\n');
+      } catch(e) {
+        console.error('Error validating ' + name);
+        throw(e);
+      }
+    }
+  })
     
 })
