6adad98f10eb9a269fe05dc45ee9c98344ce067b
Justin <justincyork@gmail.com>
2015-03-18 21:25:54 +0000

    try partials

--- test/index.js
+++ test/index.js
@@ -3,20 +3,26 @@ var data = require('../'),
 
 describe('fs-check-en', function(){
     
-  it('proper number of checks', function(){
-    expect(Object.keys(data).length).to.equal(38);
+  it('en', function(){
+    expect(data.code).to.equal('en');
   })
   
   it('all checks have a title and description', function(){
-    for(var name in data){
+    for(var name in data.checks){
       try {
-        expect(data[name].title).to.have.length.above(1);
-        expect(data[name].description).to.have.length.above(1);
-        expect(data[name].description).to.contain('\n');
+        expect(data.checks[name].title).to.have.length.above(1);
+        expect(data.checks[name].description).to.have.length.above(1);
+        expect(data.checks[name].description).to.contain('\n');
       } catch(e) {
         console.error('Error validating ' + name);
         throw(e);
       }
+    }
+  })
+    
+  it('partials loaded properly', function(){
+    for(var name in data.partials){
+      expect(data.partials[name]).to.have.length.above(1);
     }
   })
     
