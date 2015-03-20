5e9e8965dfe8ade0791fbf1b10f4dec8708cd789
Justin <justincyork@gmail.com>
2015-03-20 22:07:08 +0000

    change lang api again

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 var fs = require('fs'),
     data = require('./lib/index.js'),
     header = '(function(){var data = ',
-    footer = ';FSCheck.addLanguage(data);}())';
+    footer = ';FSCheck.language(data);}())';
 
 fs.writeFileSync('fs-check-en.js', header + JSON.stringify(data) + footer);
